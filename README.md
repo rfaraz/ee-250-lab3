c707b651a8c4000600b267ac31c8f12a0121563e
rfaraz <ridafaraz122@gmail.com>
2025-09-16 17:52:30 -0700

    mail flask app created

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ee-250-lab3
+
+***What is REST?***
+
+## Question 1: Why are RESTful APIs scalable? ##
+
+## Question 2: According to the definition of “resources” provided in the AWS article above, What are the resources the mail server is providing to clients? ##
+
+## Question 3: According to the definition of “resources” provided in the AWS article above, What are the resources the mail server is providing to clients? ##
+
+## Question 4: Why are API keys used for many RESTful APIs? What purpose do they serve? Make sure to cite any online resources you use to answer this question! ##
+
---
+++ mail_server.py
@@ -0,0 +1,186 @@
+from typing import Dict, List, Optional
+from flask import Flask, request, jsonify
+import pathlib
+import uuid
+import json
+
+
+app = Flask(__name__)
+thisdir = pathlib.Path(__file__).parent.absolute() # path to directory of this file
+
+# Function to load and save the mail to/from the json file
+
+def load_mail() -> List[Dict[str, str]]:
+    """
+    Loads the mail from the json file
+
+    Returns:
+        list: A list of dictionaries representing the mail entries
+    """
+    try:
+        return json.loads(thisdir.joinpath('mail_db.json').read_text())
+    except FileNotFoundError:
+        return []
+
+def save_mail(mail: List[Dict[str, str]]) -> None:
+    """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Writes the mail input to a new json file
+
+    Returns:
+        None
+    """
+    thisdir.joinpath('mail_db.json').write_text(json.dumps(mail, indent=4))
+
+def add_mail(mail_entry: Dict[str, str]) -> str:
+    """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Loads mail file, writes to mail file, generates a unique identifier, and saves the mail entry
+
+    Returns: 
+        str: A unique id for newly added mail entry
+    """
+    mail = load_mail()
+    mail.append(mail_entry)
+    mail_entry['id'] = str(uuid.uuid4()) # generate a unique id for the mail entry
+    save_mail(mail)
+    return mail_entry['id']
+
+def delete_mail(mail_id: str) -> bool:
+    """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Loads mail file, removes each entry from loaded mail file, and resaves empty output, clearing the mail
+
+    Returns:
+        bool: A flag determining whether the operation was successful or unsuccessful
+    """
+    mail = load_mail()
+    for i, entry in enumerate(mail):
+        if entry['id'] == mail_id:
+            mail.pop(i)
+            save_mail(mail)
+            return True
+
+    return False
+
+def get_mail(mail_id: str) -> Optional[Dict[str, str]]:
+    """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Given a mail ID number, this retrieves the text of the mail file
+
+    Returns:
+        Dict: In the case that the corresponding mail file is found, the content of the mail is returned.
+    """
+    mail = load_mail()
+    for entry in mail:
+        if entry['id'] == mail_id:
+            return entry
+
+    return None
+
+def get_inbox(recipient: str) -> List[Dict[str, str]]:
+    """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Given a specified user, loads all the mail items sent to that user. 
+
+    Returns:
+        List: A list of all mail files sent to the specified user
+    """
+    mail = load_mail()
+    inbox = []
+    for entry in mail:
+        if entry['recipient'] == recipient:
+            inbox.append(entry)
+
+    return inbox
+
+def get_sent(sender: str) -> List[Dict[str, str]]:
+    """TODO: fill out this docstring (using the load_mail docstring as a guide)
+    Loads all mail files sent out by a specified user.
+
+    Returns:
+        List: A list of all mail files sent out by a specified user.
+    """
+    mail = load_mail()
+    sent = []
+    for entry in mail:
+        if entry['sender'] == sender:
+            sent.append(entry)
+
+    return sent
+
+# API routes - these are the endpoints that the client can use to interact with the server
+@app.route('/mail', methods=['POST'])
+def add_mail_route():
+    """
+    Summary: Adds a new mail entry to the json file
+
+    Returns:
+        str: The id of the new mail entry
+    """
+    mail_entry = request.get_json()
+    mail_id = add_mail(mail_entry)
+    res = jsonify({'id': mail_id})
+    res.status_code = 201 # Status code for "created"
+    return res
+
+@app.route('/mail/<mail_id>', methods=['DELETE'])
+def delete_mail_route(mail_id: str):
+    """
+    Summary: Deletes a mail entry from the json file
+
+    Args:
+        mail_id (str): The id of the mail entry to delete
+
+    Returns:
+        bool: True if the mail was deleted, False otherwise
+    """
+    # TODO: implement this function
+    pass # remove this line
+
+@app.route('/mail/<mail_id>', methods=['GET'])
+def get_mail_route(mail_id: str):
+    """
+    Summary: Gets a mail entry from the json file
+
+    Args:
+        mail_id (str): The id of the mail entry to get
+
+    Returns:
+        dict: A dictionary representing the mail entry if it exists, None otherwise
+    """
+    res = jsonify(get_mail(mail_id))
+    res.status_code = 200 # Status code for "ok"
+    return res
+
+@app.route('/mail/inbox/<recipient>', methods=['GET'])
+def get_inbox_route(recipient: str):
+    """
+    Summary: Gets all mail entries for a recipient from the json file
+
+    Args:
+        recipient (str): The recipient of the mail
+
+    Returns:
+        list: A list of dictionaries representing the mail entries
+    """
+    res = jsonify(get_inbox(recipient))
+    res.status_code = 200
+    return res
+
+# TODO: implement a route to get all mail entries for a sender
+# HINT: start with soemthing like this:
+#   @app.route('/mail/sent/<sender>', ...)
+@app.route('/mail/sent/<sender>', methods=['GET']) 
+def get_sent_route(sender: str):
+    """
+    Summary: Gets all mail entries for a sender from the json file
+
+    Args:
+        sender (str): The sender of the mail
+
+    Returns:
+        list: A list of dictionaries representing the mail entries    
+    """
+    res = jsonify(get_sent(sender))
+    res.status_code = 200
+    return res
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
